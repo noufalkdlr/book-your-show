4c8491843a6c27a684d634f13bf17f4fc2a4dc97
Noufal <noufalkakdlr@gmail.com>
2026-01-22 07:27:47 +0530

    fix bug

--- users/admin.py
+++ users/admin.py
@@ -9,7 +9,5 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ["role", "is_active", "is_staff"]
     fieldsets = UserAdmin.fieldsets + (("Role Info", {"fields": ("role",)}),)
 
-    add_fieldsets = UserAdmin.add_fieldsets + (("Role Info", {"fields": ("role",)}),)
-
 
 admin.site.register(User, CustomUserAdmin)
