ad0e8fc04a08934466d5694ae61c67fadd563912
Noufal <noufalkakdlr@gmail.com>
2026-01-22 11:07:35 +0530

    add user model

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-
-class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ["email", "username", "role"]
-    list_filter = ["role", "is_active", "is_staff"]
-    fieldsets = UserAdmin.fieldsets + (("Role Info", {"fields": ("role",)}),)
-
-
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User)
